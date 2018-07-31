94774f50fa61abdb61b4dbb0f95954fe6afc4e43
Erik Ferlanti <eferlanti@tacc.utexas.edu>
2018-07-31 12:57:41 -0500

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,6 +11,7 @@ public class App
     public App() {}
 
     public static void main(String[] args) {
+        System.out.println("Classpath: " + System.getProperty("java.class.path"));
         System.out.println(new App().getMessage());
     }
 
